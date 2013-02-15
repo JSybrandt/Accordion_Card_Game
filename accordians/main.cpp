3c8999a0c31e1feaa7dec58b4f906e18a960d037
Justin "Nalta" Sybrandt <SybrandtJG1@gcc.edu>
2013-02-15 16:57:42 -0500

    cards go flippin cray

--- accordians/main.cpp
+++ accordians/main.cpp
@@ -9,6 +9,25 @@ using namespace std;
 
 int main()
 {
+	Card c(ACE, HEARTS);
+	while(next_suit(c.get_suit()) != END_OF_SUITS)
+	{
+		c.set_rank(ACE);
+		while(next_rank(c.get_rank()) != END_OF_RANKS)
+		{
+			int x = rand()%80;
+			int y = rand()%5;	
+			while(x<80&&y<14)
+			{
+				c.sketch(x,y,true,false);
+				x++;
+				y++;
+				Sleep(7);
+			}
+			c.set_rank(next_rank(c.get_rank()));
+		}
+		c.set_suit(next_suit(c.get_suit()));
+	}
 	return 0;
 }
 
