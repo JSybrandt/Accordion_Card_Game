d6700d3ca699ede2330c835d145d3537c849ccdc
Justin Sybrandt <SybrandtJG1@gcc.edu>
2013-03-08 12:22:01 -0500

    cut depenancies from main

--- accordians/main.cpp
+++ accordians/main.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <Windows.h>
-#include <vector>
 #include <ctime>
-#include "card.h"
-#include "deck.h"
-#include "accordion.h"
 #include "game.h"
 using namespace std;
 
